8969754bae820ca60c04caf68eded02c56b45404
qtshaimanga <q.tshaimanga@gmail.com>
2017-05-12 14:37:19 +0200

    update App with stylus

--- src/App.vue
+++ src/App.vue
@@ -68,32 +68,29 @@ export default {
 }
 </script>
 
-<style lang="scss">
-  html{
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
+<style lang="stylus">
+  html
+    width 100%
+    height 100%
+    margin 0px
+    padding 0px
 
-  body{
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
+  body
+    width 100%
+    height 100%
+    margin 0px
+    padding 0px
 
-  #app {
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
+  #app
+    width 100%
+    height 100%
+    margin 0px
+    padding 0px
 
-  .app{
-    width: 100%;
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
+  .app
+    width 100%
+    height 100%
+    margin 0px
+    padding 0px
+
 </style>
